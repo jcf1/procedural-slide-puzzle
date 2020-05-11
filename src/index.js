50c4fd2bfa6eb926fda607e75eb97d3397e8c537
unknown <john_free@aol.com>
2020-05-11 00:13:38 -0500

    Fixing Github Pages

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Game from './components/Game.jsx';
 
 const root = document.getElementById('root');
 
 render(
   <div>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div>
         <header>
           <h1>Procedural Slide Puzzles</h1>
         </header>
       </div>
       <Game/>
-    </HashRouter>
+    </BrowserRouter>
   </div>
 , root);
